892a17d3c7b51152c3574e19fd2b981434c219c3
user <user@DESKTOP-MA34BED>
2025-02-03 16:15:52 +0900

    commit

--- TCP_Fighter_report/Session.h
+++ TCP_Fighter_report/Session.h
@@ -51,8 +51,8 @@ public:
 
 	Player() {
 		_hp = dfHP;
-		_x = 6200;	//rand() % 6400;
-		_y = 6200;	//rand() % 6400;
+		_x = rand() % 6400;
+		_y = rand() % 6400;
 		_direction = (rand() % 2) * 4; //LL == 0, RR == 4
 		_move = false;
 		_ID = playerIdex;
@@ -60,8 +60,8 @@ public:
 
 	Player(Session* pSes) {
 		_hp = dfHP;
-		_x = 6200;	//rand() % 6400;
-		_y = 6200;	//rand() % 6400;
+		_x = rand() % 6400;
+		_y = rand() % 6400;
 		_direction = (rand() % 2) * 4; //LL == 0, RR == 4
 		_move = false;
 		_ID = playerIdex;
@@ -74,7 +74,7 @@ public:
 	bool Move();
 	void MoveStop(int Dir, int x, int y);
 
-	//공격 (3가지)
+	
 
 
 };
